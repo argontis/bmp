45fefd34384e03dd2d9e63a2c69fcd2e572c1941
argontis <argontis@example.com>
2026-09-17 13:58:43 +0700

    chore: cleanup temporary scripts and ignore deploy.sh

--- patch_routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$web = file_get_contents('routes/web.php');
-$target = "Route::get('/history', [DashboardController::class, 'history'])->name('history');";
-$replace = $target."\n    Route::get('/user/laporan', [ReportController::class, 'userIndex'])->name('user.laporan');";
-$web = str_replace($target, $replace, $web);
-file_put_contents('routes/web.php', $web);
-echo "routes patched\n";
